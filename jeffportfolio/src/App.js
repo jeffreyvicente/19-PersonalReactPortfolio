22e1d2aa25108444b140e5996f7b5cdfa5afacec
Jeffrey V <jeffreyvicente@hotmail.com>
2023-08-08 20:12:30 -0700

    Routed so that the Protfoilo container is called and rendered rather than each page/component

--- jeffportfolio/src/App.js
+++ jeffportfolio/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import About from './components/About';
-import Header from './components/Header'
+//import logo from './logo.svg';
+//import './App.css';
+//import Nav from './components/Nav';
+//import Footer from './components/Footer';
+//import About from './components/pages/About';
+//import Contact from './components/pages/Contact'
 
-function App() {
+//import Header from './components/Header';
+import PortfolioContainer from "./components/PortfolioContainer";
+
+
+
+const App = () => <PortfolioContainer />;
+/*function App() {
   return (
     <div className="App">
       <Nav />
       <Header/>
+      <Contact />
   
       
 
@@ -17,10 +24,12 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
 
 /*
+
+  <About/>
 
 function App() {
   return (
